71e33dd6a649718228518d7ff73d461088dfbb37
Canaan White <w_canaan@yahoo.com>
2026-08-27 17:38:24 -0700

    added the session static class

---
+++ C969-Project/Database/Session.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C969_Project.Database
+{
+    public static class Session
+    {
+        public static string CurrentUserName { get; private set; } = string.Empty;
+        public static int CurrentUserId { get; private set; }
+
+        public static void Start(string username, int userId)
+        {
+            CurrentUserId = userId;
+            CurrentUsername = username;
+        }
+
+        public static void Clear()
+        {
+            CurrentUserId = 0;
+            CurrentUsername = string.Empty;
+        }
+    }
+}
